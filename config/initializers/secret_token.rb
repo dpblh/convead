93cc389371266022478133dc04ce199fc09c9d90
tim <bajurovt@gmail.com>
2014-07-19 21:37:36 +0400

    start development

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Convead::Application.config.secret_key_base = 'e91b932e93fbcd186ba7e46b80dba9124d22de5e55bc4754220f077fb9093f4d85884d1a20f8b5910e70b3101f4174f4830adc029f3794ad907fce1017036fa7'
