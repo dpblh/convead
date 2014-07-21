24952985cbb4359ffbc33292f6687b6b519dd9c7
tim <bajurovt@gmail.com>
2014-07-21 19:59:58 +0400

    final

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,13 +2,11 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy, :similar]
 
   # GET /products
-  # GET /products.json
   def index
     @products = Product.all
   end
 
   # GET /products/1
-  # GET /products/1.json
   def show
   end
 
@@ -19,59 +17,9 @@ class ProductsController < ApplicationController
     @timesql = "#{end0 - start} время запроса";
     # render :show
     respond_to do |format|
-      format.json {render :json => @products}
       format.html {render action: :index, notice: "Similar #{end0 - start}"}
     end 
   end
-
-  # GET /products/new
-  # def new
-    # @product = Product.new
-  # end
-
-  # GET /products/1/edit
-  # def edit
-  # end
-
-  # POST /products
-  # POST /products.json
-  # def create
-  #   @product = Product.new(product_params)
-
-  #   respond_to do |format|
-  #     if @product.save
-  #       format.html { redirect_to @product, notice: 'Product was successfully created.' }
-  #       format.json { render action: 'show', status: :created, location: @product }
-  #     else
-  #       format.html { render action: 'new' }
-  #       format.json { render json: @product.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # PATCH/PUT /products/1
-  # PATCH/PUT /products/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @product.update(product_params)
-  #       format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-  #       format.json { head :no_content }
-  #     else
-  #       format.html { render action: 'edit' }
-  #       format.json { render json: @product.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # DELETE /products/1
-  # DELETE /products/1.json
-  # def destroy
-  #   @product.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to products_url }
-  #     format.json { head :no_content }
-  #   end
-  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
