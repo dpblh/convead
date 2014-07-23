0aea45a35af1e866caf6c4396c2cb92dcd0bd976
tim <bajurovt@gmail.com>
2014-07-23 19:51:01 +0400

    generate

--- generate.rb
+++ generate.rb
@@ -4,9 +4,12 @@ print 'start'
 
 def generate
 begin
+prop = Property.all
+rand = Random.new
+taxon = Taxon.find 121
 (1..90000).to_a.each {|i| prop1 = (1..10).map{|e| ProductProperty.create(property: prop.sample, num_value: rand.rand(6..6000))}; Product.create(name: :some, taxon: taxon, product_properties: prop1)}
-rescue Exception => ex
-	print ex
+rescue Exception => exc
+generate if Product.count < 100000
 end
 end
 
